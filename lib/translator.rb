027a70db5b45800fcdd2e6bef564b71650f1a5a4
Lauren Cunningham <lauren.busbee@gmail.com>
2020-08-07 01:29:58 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -18,6 +18,7 @@ end
 
 def get_japanese_emoticon(library, english_emoticon)
   
+<<<<<<< HEAD
   library = load_library(library)
   library.each do |value, meaning|
        if library[value][:english] == english_emoticon
@@ -25,10 +26,19 @@ def get_japanese_emoticon(library, english_emoticon)
    end
   end
   return "Sorry, that emoticon was not found" 
+=======
+  library = load_library(file)
+  library.each do |value, meaning|
+       if value[:english] == english_emoticon
+         return meaning
+   end
+  end
+>>>>>>> 5bda20d21a95ff96549301c7a30ea7d1e4728bb5
 end
 
 def get_english_meaning(library, japanese_emoticon)
  
+<<<<<<< HEAD
   library = load_library(library)
    library.each do |value, meaning|
        if library[value][:japanese] == japanese_emoticon
@@ -36,4 +46,12 @@ def get_english_meaning(library, japanese_emoticon)
       end
     end
     return "Sorry, that emoticon was not found" 
+=======
+  library = load_library(file)
+   library.each do |value, meaning|
+       if value[:japanese] == japanese_emoticon
+         return meaning
+      end
+    end
+>>>>>>> 5bda20d21a95ff96549301c7a30ea7d1e4728bb5
   end
